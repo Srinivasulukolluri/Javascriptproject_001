e874347a8b8feb459daadc45d4d6ded0637f5bd9
srinivas <srinivasulukolluri@hotmail.com>
2023-03-03 11:31:40 -0800

    first commit

---
+++ demo/src/main/java/javascript_html_001/App.java
@@ -0,0 +1,13 @@
+package javascript_html_001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
